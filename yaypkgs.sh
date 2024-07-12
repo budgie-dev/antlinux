613922383386642ccf80b0b0890acf00eafd7899
budgie-dev <166736447+budgie-dev@users.noreply.github.com>
2024-07-12 16:38:16 +0000

    Create yaypkgs.sh

---
+++ yaypkgs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yay -S freetube librewolf-bin tk lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings galculator flameshot terminus-font alsa-utils xfce4 lolcat blueman bluez bluez-utils gnome-disk-utility neofetch rofi
