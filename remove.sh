45a1dfd6b33d820191edf8c5e24e1075ca2b2da0
budgie-dev <166736447+budgie-dev@users.noreply.github.com>
2024-07-12 16:37:40 +0000

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf /home/fuckubuntu/.config/neofetch ~/.config/rofi
+mkdir /home/fuckubuntu/.config/neofetch
+mkdir /home/fuckubuntu/.config/rofi
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-screensaver.xml
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+rm -rf /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
