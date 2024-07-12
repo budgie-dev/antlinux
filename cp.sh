2061ecc3db039dbba4c55deee7b33a1d2ccfd44f
budgie-dev <166736447+budgie-dev@users.noreply.github.com>
2024-07-12 16:37:16 +0000

    Create cp.sh

---
+++ cp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cp -r neofetch /home/fuckubuntu/.config/
+cp -r rofi /home/fuckubuntu/.config/
+mkdir /etc/dotfiles/
+cp -r wallpapers /etc/dotfiles/
+cp .bashrc /home/fuckubuntu/
+cp -r rose-pine-gtk /usr/share/themes/
+
+cp xfce4-desktop.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-keyboard-shortcuts.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-panel.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-power-manager.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-screensaver.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-terminal.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfwm4.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xsettings.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
