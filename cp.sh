dc8a91b9c7909b125ffaaee783ae3c3a24b09629
budgie-dev <arcigg@proton.me>
2024-07-17 13:09:29 +0200

    clean up

--- cp.sh
+++ cp.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cp -r neofetch /home/fuckubuntu/.config/
-cp -r rofi /home/fuckubuntu/.config/
+cp -r neofetch /home/burnmybread/.config/
+cp -r rofi /home/burnmybread/.config/
 mkdir /etc/dotfiles/
 cp -r wallpapers /etc/dotfiles/
-cp .bashrc /home/fuckubuntu/
+cp .bashrc /home/burnmybread/
 cp -r rose-pine-gtk /usr/share/themes/
 
-cp xfce4-desktop.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp xfce4-keyboard-shortcuts.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp xfce4-panel.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp xfce4-power-manager.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp xfce4-screensaver.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp xfce4-terminal.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp xfwm4.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp xsettings.xml /home/fuckubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-desktop.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-keyboard-shortcuts.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-panel.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-power-manager.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-screensaver.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfce4-terminal.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xfwm4.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp xsettings.xml /home/burnmybread/.config/xfce4/xfconf/xfce-perchannel-xml/
--- yaypkgs.sh
+++ yaypkgs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yay -S freetube librewolf-bin tk lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings galculator flameshot terminus-font alsa-utils xfce4 lolcat blueman bluez bluez-utils gnome-disk-utility neofetch rofi
+yay -S freetube librewolf tk lightdm-gtk-greeter-settings galculator flameshot terminus-font alsa-utils lolcat blueman bluez bluez-utils gnome-disk-utility neofetch rofi --noconfirm
